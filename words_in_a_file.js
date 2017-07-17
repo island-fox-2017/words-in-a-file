05e4a82bbf3ad8420832858106b716be39f5bbe9
achimbaggins <achim_baggins@yahoo.com>
2017-07-17 14:46:47 +0700

    done

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -2,8 +2,29 @@ var sys = require('sys');
 const fs = require('fs')
 
 // actual conversion code starts here
-function words_in_a_file(filename, limit) {
+function byLength( e1 , e2 ) {
+  e1 = e1.toString().length;
+  e2 = e2.toString().length;
+  return e1<e2 ? 1 : ( e1>e2 ? -1 : 0 );
 }
+
+function words_in_a_file(filename, limit) {
+  let result = []
+  let arrNormalisasi = []
+  let fileContent = fs.readFileSync(filename, 'utf8');
+  let split = fileContent.replace(/[^a-z0-9]/gmi, " ").replace(/\s+/g, " ").split(' ');
+
+  let sorted = split.sort(byLength)
+
+  for(let i = 0; i < limit; i++){
+    result.push(sorted[i])
+  }
+  return result.join(' ');
+
+}
+
+
+console.log(words_in_a_file('source.txt', 3));
 
 module.exports = {
   words_in_a_file: words_in_a_file
